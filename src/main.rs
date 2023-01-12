bfda23a623bbc7a63b112776e5315a41dda818b6
Hopper <Glenn.Hopper@signetjewelers.com>
2023-01-12 13:29:44 -0500

    A little more progress.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use rsautogui::mouse;
 // use rsautogui::keyboard;
-use rsautogui::screen;
 use std::thread::sleep;
 use std::time::Duration;
 use rand::Rng;
@@ -8,10 +7,17 @@ use hotkey;
 use screenshots::Screen;
 use std::{fs, time::Instant};
 
+fn print_pixel_at_mouse() {
+    // get mouse position
+    let (mouse_x, mouse_y) = mouse::position();
+    println!("Mouse is at ({}, {})", mouse_x, mouse_y);
+}
+
 fn wiggle_mouse() {
     // print the current mouse position
     let (mouse_x, mouse_y) = mouse::position();
     println!("Mouse is at ({}, {})", mouse_x, mouse_y);
+    // print the color of the pixel under the mouse
     let mut random_generator = rand::thread_rng();
     let mut x:u16;
     let mut y:u16;
@@ -29,39 +35,26 @@ fn wiggle_mouse() {
     // convert ITERATIONS to f64
     let iterations_as_float:f64 = ITERATIONS as f64;
     println!("That's {} wiggles per second", iterations_as_float/elapsed_time);
-    // save a screenshot
-    // make a tuple of (0, 0, 500, 500) in u16 format
-    let x:u16 = 0;
-    let y:u16 = 0;
-    let w:u16 = 500;
-    let h:u16 = 500;
-    let screenshot = screen::screenshot(x, y, w, h);
-    screen::printscreen(&screenshot, "screenshot.png");
 }
 
 
 
 fn take_screenshot() {
   let start = Instant::now();
-  let screens = Screen::all().unwrap();
-
-  for screen in screens {
-    println!("capturer {:?}", screen);
-    let mut image = screen.capture().unwrap();
-    let mut buffer = image.buffer();
-    fs::write(format!("target/{}.png", screen.display_info.id), &buffer).unwrap();
-
-    image = screen.capture_area(300, 300, 300, 300).unwrap();
-    buffer = image.buffer();
-    fs::write(format!("target/{}-2.png", screen.display_info.id), &buffer).unwrap();
-  }
-
-  let screen = Screen::from_point(100, 100).unwrap();
+  // grab the first screen
+  let screen = Screen::all().unwrap()[0];
+  // if there's no folder called screenshots, create it
+  fs::create_dir_all("screenshots").unwrap();
   println!("capturer {:?}", screen);
+  // let mut image = screen.capture().unwrap();
+  let image = screen.capture_area(0, 0, 200, 200).unwrap();
 
-  let image = screen.capture_area(300, 300, 300, 300).unwrap();
-  let buffer = image.buffer();
-  fs::write("target/capture_display_with_point.png", &buffer).unwrap();
+  let mut buffer = image.buffer();
+  // print the first 10 bytes of the buffer
+  println!("buffer: {:?}", &buffer[0..10]);
+  fs::write(format!("screenshots/{}.png", screen.display_info.id), &buffer).unwrap();
+  // print the number of bytes in the buffer
+  println!("buffer size: {}", buffer.len());
 
   println!("Time taken: {:?}", start.elapsed());
 }
@@ -73,7 +66,7 @@ fn main() {
     hk.register_hotkey(
         hotkey::modifiers::CONTROL | hotkey::modifiers::SHIFT,
         'A' as u32,
-        || wiggle_mouse(),
+        || take_screenshot(),
     )
     .unwrap();
 
